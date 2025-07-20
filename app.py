8f18b56e4d7ebddba52e3103ac035e5575799667
sahil095 <sehgal.sahil0786@gmail.com>
2025-07-20 02:40:49 -0400

    fast API integration

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+from fastapi import FastAPI
+from pydantic import BaseModel
+from agents.agentic_workflow import GraphBuilder
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+class QueryRequest(BaseModel):
+    query: str
+
+@app.post("/query")
+async def query_travel_agent(query: QueryRequest):
+    try:
+        print(query)
+        graph = GraphBuilder(model_provder = "groq")
+        react_app = graph()
+
+        png_graph = react_app.get_graph().draw_mermaid_png()
+        with open("my_graph.png", "wb") as f:
+            f.write(png_graph)
+
+        print(f"Graph saved as 'my_graph.png' in {os.getcwd()}")
+
+        messages = {"messages": [query.question]}
+        output = react_app.invoke(messages)
+
+        if isinstance(output, dict) and "messages" in output:
+            final_output = output['messages'][-1].content
+        else:
+            final_output = str(output)
+        
+        return {"answer": final_output}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={'error': str(e)})
