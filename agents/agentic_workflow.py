124aaba02e18515b07893c26b29ad060095f2103
sahil095 <sehgal.sahil0786@gmail.com>
2025-07-20 02:24:43 -0400

    prompt and agent workflow

--- agents/agentic_workflow.py
+++ agents/agentic_workflow.py
@@ -1,6 +1,6 @@
 from utils.model_loaders import ModelLoader
 from prompt_library.prompts import SYSTEM_PROMPT
-from langgraph.graph import StateGraph, MessageState, END, START
+from langgraph.graph import StateGraph, MessagesState, END, START
 from langgraph.prebuilt import ToolNode, tools_condition
 
 
@@ -14,13 +14,31 @@ from langgraph.prebuilt import ToolNode, tools_condition
 class GraphBuilder():
 
     def __init__(self):
-        pass
+        self.tools = [
 
-    def agent_function(self):
-        pass
+        ]
+
+        self.system_prompt = SYSTEM_PROMPT
+
+    def agent_function(self, state:MessagesState):
+        """ Main Agent Function """
+        user_question = state['messages']
+        input_question = [self.system_prompt] + user_question
+        response = self.llm_with_tools.invoke(input_question)
+        return {"messages":[response]}
 
     def build_graph(self):
-        pass
+        graph_builder = StateGraph(MessagesState)
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
+        graph_builder.add_edge(START, "agent")
+        graph_builder.add_conditional_edges("agent", tools_condition)
+        graph_builder.add_edge("tools", "agent")
+        graph_builder.add_edge("agent", END)
+
+        self.graph = graph_builder.compile()
+
+        return self.graph
 
     def __call__(self, *args, **kwds):
-        pass
+        return self.build_graph()
