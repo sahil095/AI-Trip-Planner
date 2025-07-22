23d8c2f8fb4d947ed8ad12af19683c0b13538591
sahil095 <sehgal.sahil0786@gmail.com>
2025-07-22 00:18:20 -0400

    model loader file fixed

--- utils/model_loaders.py
+++ utils/model_loaders.py
@@ -7,45 +7,39 @@ from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 
 class ConfigLoader:
-
     def __init__(self):
-        print(f"Loaded Config...")
-        self.cofig = load_config()
-
+        print(f"Loaded config.....")
+        self.config = load_config()
+    
     def __getitem__(self, key):
-        return self.cofig[key]
-
+        return self.config[key]
 
 class ModelLoader(BaseModel):
-
     model_provider: Literal["groq", "openai"] = "groq"
     config: Optional[ConfigLoader] = Field(default=None, exclude=True)
-    
-    def model_post_init(self, __context:Any) -> None:
+
+    def model_post_init(self, __context: Any) -> None:
         self.config = ConfigLoader()
-
-    class Config:
-        arbitary_types_allowed = True
-
     
-    def load_ll(self):
+    class Config:
+        arbitrary_types_allowed = True
+    
+    def load_llm(self):
         """
-        Load and return the LLM Model
+        Load and return the LLM model.
         """
-
         print("LLM loading...")
         print(f"Loading model from provider: {self.model_provider}")
-
         if self.model_provider == "groq":
-            print("Loading LLM from groq.........")
+            print("Loading LLM from Groq..............")
             groq_api_key = os.getenv("GROQ_API_KEY")
             model_name = self.config["llm"]["groq"]["model_name"]
-            llm = ChatGroq(model=model_name, api_key=groq_api_key)
+            llm=ChatGroq(model=model_name, api_key=groq_api_key)
         elif self.model_provider == "openai":
-            print("Loading LLM from OpenAI................")
+            print("Loading LLM from OpenAI..............")
             openai_api_key = os.getenv("OPENAI_API_KEY")
-            model_name = self.config['llm']['openai']['model_name']
-            llm = ChatOpenAI(model_name=model_name, api_key=openai_api_key)
+            model_name = self.config["llm"]["openai"]["model_name"]
+            llm = ChatOpenAI(model_name="o4-mini", api_key=openai_api_key)
         
         return llm
 
